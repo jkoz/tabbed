94b158b2cb1c143f204b6fe413558d1633508f73
jkoz <phuoctaitp@gmail.com>
2014-12-02 22:19:41 -0700

    use solarized color

--- config.def.h
+++ config.def.h
@@ -2,10 +2,10 @@
 
 /* appearance */
 static const char font[]        = "-*-*-medium-*-*-*-14-*-*-*-*-*-*-*";
-static const char* normbgcolor  = "#222222";
-static const char* normfgcolor  = "#cccccc";
-static const char* selbgcolor   = "#555555";
-static const char* selfgcolor   = "#ffffff";
+static const char* normbgcolor  = "#002b36";
+static const char* normfgcolor  = "#bbbbbb";
+static const char* selbgcolor   = "#073642";
+static const char* selfgcolor   = "#eeeeee";
 static const char* urgbgcolor   = "#111111";
 static const char* urgfgcolor   = "#cc0000";
 static const char before[]      = "<";
